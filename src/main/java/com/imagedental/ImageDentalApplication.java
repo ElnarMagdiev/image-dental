3ec3bbad9495f8aee51979dbe5e7478e9c50f7e5
Магдиев <elnarmag263@gmail.com>
2020-08-26 11:09:14 +0300

    first commit

---
+++ src/main/java/com/imagedental/ImageDentalApplication.java
@@ -0,0 +1,13 @@
+package com.imagedental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageDentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageDentalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/imagedental/entity/User.java
@@ -0,0 +1,4 @@
+package com.imagedental.entity;
+
+public class User {
+}
